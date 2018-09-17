3fd991f21ba02b466703523747dd742c904cff69
Joseph Lane <jlane.pro@gmail.com>
2018-09-16 19:20:59 -0500

    Cleanup

--- topcoder/DegreesToRadians.py
+++
@@ -1,8 +0,0 @@
-import math
-
-class DegreesToRadians:
-	def convertToRadians(self, degrees, minutes, seconds):
-		return math.radians(degrees + (minutes / 60.0) + (seconds / 3600.0))
-
-s = DegreesToRadians()
-print s.convertToRadians(359, 59, 59)
--- topcoder/TagalogDictionary.py
+++
@@ -1,8 +0,0 @@
-class TagalogDictionary:
-	def sortWords(self, words):
-		a = list(words)
-		a.sort(key = ["C", "B", "A"])
-		return tuple(a)
-
-p = TagalogDictionary()
-print p.sortWords({"knilngiggnngginggn","ingkigningg","kingkong","dingdong","dindong","dingdont","ingkblot"})
